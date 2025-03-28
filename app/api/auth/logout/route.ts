0b11fb319cb105aa67be102cfa08fd687b58c092
Nendow <43798922+Nendow@users.noreply.github.com>
2025-03-28 15:35:04 +0000

    Update route.ts

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,6 +1,8 @@
 "use client"
+// The runtime must be a string, not a function
 export const runtime = "edge"
 
+// Fix the import to match our updated auth.ts file
 import { logout } from "@/lib/auth"
 
 export async function POST() {
